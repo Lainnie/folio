e7fbbe64749f651d1987879110d91184c5f41764
Lainnie <l.diallo.pro@gmail.com>
2013-08-14 16:59:24 +0200

    press enter message

--- static/js/script.js
+++ static/js/script.js
@@ -1,15 +1,19 @@
 (function(){
 	'use_strict';
 
-	$.vegas('slideshow', {
-		delay: 8000,
-		backgrounds: [
-    		{src: 'static/img/4.jpg', fade: 6000},
-    		{src: 'static/img/4.jpg', fade: 6000},
-    		{src: 'static/img/4.jpg', fade: 6000},
-    		{src: 'static/img/4.jpg', fade: 6000},
-		]
-  	})('overlay', {
-    	src:'static/lib/vegas/overlays/15.png'
-  	});
-});
+	//$.vegas('slideshow', {
+		//delay: 8000,
+		//backgrounds: [
+            //{src: 'static/img/4.jpg', fade: 6000},
+            //{src: 'static/img/4.jpg', fade: 6000},
+            //{src: 'static/img/4.jpg', fade: 6000},
+            //{src: 'static/img/4.jpg', fade: 6000},
+		//]
+      //})('overlay', {
+        //src:'static/lib/vegas/overlays/15.png'
+      //});
+
+    setTimeout(function(){
+        $('#notif').fadeOut('slow');
+    }, 4000);
+})();
